ea0675f5c5ae9875b094b5929a1ff87d17418f2b
jangsujae/201311240 <sujaes@naver.com>
2016-09-21 14:45:03 +0900

    Add files via upload

---
+++ weight.java
@@ -0,0 +1,11 @@
+class Person{
+private float weight;
+public void setWeight(float w){
+this.weight = w;
+}
+private float getWeight(){
+return weight;
+}
+}
+Person sujae = new Person();
+sujae.setWeight((float)65.5)
